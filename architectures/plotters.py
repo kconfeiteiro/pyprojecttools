d57f85d2671148456e02b4097f70374b273e7d6c
kconfeiteiro <confeitk@outlook.com>
2023-08-22 21:46:56 -0400

    Renamed data_io_operations.py and directory_operations.py to datahelpers.py and dirhelpers.py, respectively; added userwarnings.py; added abstract library for plotting base classes (see plotters.py); and modified helper utilities in datatools.py, listtools.py, and strtools.py

---
+++ architectures/plotters.py
@@ -0,0 +1,36 @@
+"""Abstract base classes for plotting objects."""
+
+from abc import ABC, abstractmethod, abstractproperty
+
+
+class StandardPlot(ABC):
+    """
+    Standard abastract base class for generic plots.
+
+	Method:
+		prepdata: Prepare data.
+		plot: Executes plotting code.
+		savefig: Saves figure(s).
+
+	Properties:
+		figure: Returns figure object.
+
+    Inheritance:
+        ABC: Abstract Base Class.
+    """
+
+    @abstractmethod
+    def prepdata(self):
+        pass
+
+    @abstractmethod
+    def plot(self):
+        pass
+
+    @abstractmethod
+    def savefig(self):
+        pass
+
+    @abstractproperty
+    def figure(self):
+        pass
--- utilities/datatools.py
+++ utilities/datatools.py
@@ -1,11 +1,54 @@
-from typing import Any, List, Tuple, Union
-
-
 """ Tools related to data manipulation/operations """
 
 
+import warnings
+from typing import Any, List, Literal, Tuple, Union
+
+from pandas import DataFrame
+
+from userwarnings import MissingArgumentsWarning
+
+
+def reduce_df(
+    data: DataFrame = None,
+    percent: float = None,
+    save_type: Literal["list", "tuple"] = None,
+) -> DataFrame | List[DataFrame] | Tuple[DataFrame]:
+    """
+
+    Reduces size of `pd.DataFrame` input based on decimal-representation of what percentage of the original dataset the returned data set should be. Orginial dataset will be returned if parameters are not correctly set (as a safeguard). User is warned if arguments are missing.
+
+    Additional notes:
+    - Your data can be returned as a list or tuple of the columns
+
+    Parameters
+    ----------
+    data : DataFrame, optional
+        Original dataset, by default None
+    percent : float, optional
+        Percentage of `data` that you want returned, by default None
+
+    Returns
+    -------
+    DataFrame
+        Shortened dataframe.
+    """
+    if not (data or percent):
+        warnings.warn(
+            "'data' and 'percent' must both me used. Original dataset will be returned if inputted, otherwise, None will be returned.",
+            MissingArgumentsWarning,
+        )
+        return data if data and not percent else None
+    elif data and percent:
+        new_df = data.head(int(data.shape[0] * percent))
+        cols_list = [new_df[col] for col in new_df.columns]
+        returns = {"list": cols_list, "tuple": tuple(cols_list)}
+        return returns.get(save_type, new_df)
+
+
 def pull_columns(data: Any = None, *cols, as_tuple: bool = False) -> Union[List, Tuple]:
-    """Method for pulling specific columns of dataframe, as list or tuple of said columns.
+    """
+    Method for pulling specific columns of dataframe, as list or tuple of said columns.
 
     Args:
         data (Any, optional): data to pull from (as a Pandas dataframe). Defaults to None.
