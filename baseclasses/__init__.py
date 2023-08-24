abc7511c324b353aa1bbc6abe9c3ec0a98ccc375
kconfeiteiro <confeitk@outlook.com>
2023-08-24 14:17:44 -0400

    Added more methods to `Plot` abstract base class

--- baseclasses/__init__.py
+++ baseclasses/__init__.py
@@ -1,6 +1,8 @@
 """Module for abstract classes (architectures) that are common and uncommon, for general usage."""
 
-from typing import Iterable
+from dataclasses import dataclass
+from enum import Enum
+from typing import Iterable, TypedDict
 
 
 class ObjIterator:
@@ -28,3 +30,23 @@ class ObjIterator:
             raise StopIteration
         self.index += 1
         return self.items[self.index]
+
+
+class RANGE(Enum):
+    """
+    Value range for data reduction.
+
+    Value range:
+    - (low) `RANGE.low` = 0.25
+    - (med) `RANGE.med` = 0.5
+    - (high) `RANGE.high` = 0.75
+    """
+    low = 0.25
+    med = 0.5
+    high = 0.75
+
+
+class PltCfg(TypedDict):
+    size: str
+    save_as: str
+    reduce_data: RANGE | int | float
--- baseclasses/plotters.py
+++ baseclasses/plotters.py
@@ -1,6 +1,11 @@
 """Abstract base classes for plotting objects."""
 
 from abc import ABC, abstractmethod, abstractproperty
+from typing import Any
+
+from pandas import DataFrame
+
+from baseclasses import RANGE, PltCfg
 from utilities.datatools import reduce_df
 
 
@@ -15,10 +20,33 @@ class Plot(ABC):
 
     Properties:
         figure: Returns figure object.
+        reduce_data (RANGE values): setting to reduce data; either `.low` (25%), `.med` (50%), and `.high` (75%) of original dataaset size.
 
     Inheritance:
         ABC: Abstract Base Class.
+
+    Raises:
+        ValueError: If inputted (int or float) value is not between `(0, 1]`
     """
+
+    reduc_types = RANGE | int | float | None
+
+    def __init__(
+        self,
+        dataset: DataFrame | Any = None,
+        plt_cfg: PltCfg = None,
+        reduce_data: reduc_types = None,
+    ) -> None:
+        self.dataset = dataset
+        self.plt_cfg = plt_cfg
+        self.reduce_data = reduce_data
+
+        if self.reduce_data:
+            if isinstance(self.reduce_data, int | float) and (
+                self.reduce_data > 1 or self.reduce_data <= 0
+            ):
+                _msg = f"As an numerical value, 'reduce_data' must be between range [0, 1]. Recieved {self.reduce_data}"
+                raise ValueError(_msg)
 
     @abstractmethod
     def prepdata(self):
@@ -36,6 +64,24 @@ class Plot(ABC):
     def figure(self):
         pass
 
-    def reduce_data(self, amt: int = None):
-        self.dataset = reduce_df(self.dataset, )
-        return
+    @property
+    def dataset(self):
+        return self.dataset
+
+    @dataset.setter
+    def dataset(self, value):
+        self.dataset = value
+
+    def _reduce_data(self, amt: reduc_types = None):
+        """
+        Reduces data by `RANGE` setting or by custom size (int or float, between `0` and `1`). Private Method.
+
+        Value range:
+        - low = 0.25
+        - med = 0.5
+        - high = 0.75
+
+        Args:
+            amt (int, optional): New decimal percentage-based size of original dataset. Defaults to None.
+        """
+        self.dataset = reduce_df(self.dataset, amt)
