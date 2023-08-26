547ebb0ad20db37876fa367d284c975b92636ce7
kconfeiteiro <confeitk@outlook.com>
2023-08-25 23:56:44 -0400

    Added configuration to base __init__.py script; updated READEME.md; revamped Plot base class in plotters.py; added iterators.py; etc.

--- __init__.py
+++ __init__.py
@@ -1,50 +1,77 @@
-from enum import Enum
-from typing import Iterable, TypedDict
+"""
+### pyprojecttools - A Versatile Python Project Toolset Package
 
+PyProjectTools is a powerful open-source toolkit for Python project development. It offers essential utilities and efficient workflows for projects of all sizes, simplifying creation, organization, and management. From virtual environments and package management to testing and documentation. PyProjectTools offers helper functions, helper libraries, a collection of custom datatypes for type hinting, and more.
 
-class ObjIterator:
-    """
-    `ObjIterator`: Permits iterations through objects for child classes.
+Additional info:
+- Maintainer: Krystian Confeitiro
+- Contact: confeitk@outlook.com
 
-    Attributes:
-        items (type): Object to iterate.
-        index (type): Index counted during iteration.
+### Project tree
+```
+pyprojecttools
+├─ baseclasses
+│  ├─ datareaders.py
+│  ├─ pipelines.py
+│  ├─ plotters.py
+│  └─ __init__.py
+├─ batch_scripts
+│  ├─ clonePyProjectTools.bat
+│  ├─ tarExtraction.bat
+│  └─ update.bat
+├─ datahelpers.py
+├─ dirhelpers.py
+├─ dtypes.py
+├─ errors_exceptions.py
+├─ iterators.py
+├─ monitoring
+│  ├─ lineprofiling.py
+│  ├─ linting.py
+│  ├─ logging.py
+│  ├─ memoryprofiling.py
+│  ├─ methodprofiling.py
+│  └─ __init__.py
+├─ multimmedia
+│  ├─ photomanipulation.py
+│  └─ __init__.py
+├─ reports
+│  ├─ reportelements.py
+│  ├─ reports.py
+│  └─ __init__.py
+├─ strfmts.py
+├─ transform
+│  ├─ coordinates.py
+│  ├─ units.py
+│  └─ __init__.py
+├─ userwarnings.py
+├─ utilities
+│  ├─ datatools.py
+│  ├─ listtools.py
+│  ├─ strtools.py
+│  └─ __init__.py
+└─ __init__.py
+```
+"""
 
-    Args:
-        items (Iterable=None): Object to iterate.
+from .baseclasses import datareaders, pipelines, plotters
+from .dirhelpers import file_filterm, mkdirectory
+from .multimmedia.photomanipulation import GIF
+from .utilities import datatools, listtools, strtools
 
-    """
-
-    def __init__(self, items: Iterable = None):
-        self.items = items
-        self.index = 0
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.index >= len(self.items):
-            raise StopIteration
-        self.index += 1
-        return self.items[self.index]
-
-
-class RANGE(Enum):
-    """
-    Value range for data reduction.
-
-    Value range:
-    - (low) `RANGE.low` = 0.25
-    - (med) `RANGE.med` = 0.5
-    - (high) `RANGE.high` = 0.75
-    """
-
-    low = 0.25
-    med = 0.5
-    high = 0.75
-
-
-class PltCfg(TypedDict):
-    size: str
-    save_as: str
-    reduce_data: RANGE | int | float
+__version__ = "1.2.2"
+__all__ = [
+    "datareaders",
+    "datatools",
+    "DirectoryFileIterator",
+    "file_filterm",
+    "GIF",
+    "listtools",
+    "mkdirectory",
+    "pipelines",
+    "plotters",
+    "strtools",
+]
+__license__ = "MIT"
+__url__ = "https://github.com/kconfeiteiro/pyprojecttools"
+__email__ = "confeitk@outlook.com"
+__title__ = "PyProjectTools"
---
+++ errors_exceptions.py
@@ -0,0 +1,62 @@
+"""User defined errors and exceptions."""
+
+
+import traceback
+from typing import Any, Callable
+
+
+class NotASetError(TypeError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+def tryexcept(func):
+    def wrapper(*args, **kwargs):
+        try:
+            result = yield from func(*args, **kwargs)
+            yield result
+        except Exception as e:
+            print(e)
+            result = None
+            yield result
+        return result
+
+    return wrapper
+
+
+class IncompatibleArugmentsWarning(UserWarning):
+    pass
+
+
+class MissingArgumentError(ValueError):
+    def __init__(self, functionname: object = None) -> None:
+        super().__init__()
+        self.functionname = functionname
+        self._funcname = self.functionname.__name__
+
+    def __str__(self) -> str:
+        return
+
+
+class FileReadError(ValueError):
+    def __init__(self, filename: str = None):
+        self.filename = filename
+        self.tb = traceback.format_exc()
+        self.msg = f"Error reading file: {self.filename}"
+
+    def __str__(self) -> str:
+        return f"{self.msg}\n{self.tb}"
+
+
+class MissingMethod(Exception):
+    def __init__(self, classname: Any = None, method: Callable = None) -> None:
+        self.classname = classname.__name__
+        self.method = method.__name__
+
+    def __str__(self):
+        return f"Method {self.method} in {self.classname} must be ran prior."
+
+
+class KeyErrorWarning(UserWarning):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
